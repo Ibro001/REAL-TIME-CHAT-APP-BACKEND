b71a819bf0a943200c3ab48b1445a717beae6575
ibro001 <ibrahimmoses56@gmail.com>
2022-10-16 20:05:15 +0100

    added the setAvatar route

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { register } = require('../Controllers/userController')
-const { login } = require('../Controllers/userController')
+const { register, login,  setAvatar } = require('../Controllers/userController')
+
 
 
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/setAvatar/:id', setAvatar);
 
 module.exports = router;    
